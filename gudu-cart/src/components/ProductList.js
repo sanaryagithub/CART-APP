42a34e7a950c7469411c5844cb8db1baba145985
sanaryagithub <santoshkumaracharya64@gmail.com>
2022-08-26 00:25:37 +0530

    CART APP

---
+++ gudu-cart/src/components/ProductList.js
@@ -0,0 +1,21 @@
+
+import Product from "./Product.js";
+
+export default function ProductList(props) {
+  return props.productList.length > 0 ? (
+    props.productList.map((product, i) => {
+      return (
+        <Product
+          product={product}
+          key={i}
+          incrementQuantity={props.incrementQuantity}
+          index={i}
+          decrementQuantity={props.decrementQuantity}
+          removeItem={props.removeItem}
+        />
+      );
+    })
+  ) : (
+    <h1>Sorry no products in Gudu's Bazar</h1>
+  );
+}
